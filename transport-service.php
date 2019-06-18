713d66aeffaa2cc4b5766b99932341cb231ec55d
Dylan Bourbotte <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-06-18 15:00:29 +0200

    Ajout des pages transport_service, achat de materiel, paiment

--- transport-service.php
+++ transport-service.php
@@ -3,6 +3,15 @@ include('includes/header.php')
 ?>
 
 
+<section class="bientot">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center bientot_txt">
+                <h1>Page en cours de construction..</h1>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
